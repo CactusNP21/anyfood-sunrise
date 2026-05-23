ebcead89809a60c2a3c522a29d6459376ec14c74
nazar <nazarkucher3212@gmail.com>
2026-05-23 07:48:50 +0300

    recipe list

--- apps/anyfood/src/app/apps/recipe/features/recipe-list/recipe-list.component.ts
+++ apps/anyfood/src/app/apps/recipe/features/recipe-list/recipe-list.component.ts
@@ -2,10 +2,12 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RecipeClient } from '../../../../core/clients/recipe/recipe.client';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { AnyfoodImageComponent } from '@anyfood/ui';
+import { DecimalPipe } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RouterLink],
+  imports: [RouterLink, AnyfoodImageComponent, DecimalPipe],
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
